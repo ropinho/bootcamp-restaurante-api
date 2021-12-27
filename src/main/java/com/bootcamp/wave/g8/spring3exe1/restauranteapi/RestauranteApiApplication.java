58961b7f4b43f4b43ae84f4c2bd74e7cd6af9f1d
Ronaldd Pinho <ronaldd.pinho@mercadolivre.com>
2021-12-27 11:54:47 -0300

    bootstrap spring project

---
+++ src/main/java/com/bootcamp/wave/g8/spring3exe1/restauranteapi/RestauranteApiApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.wave.g8.spring3exe1.restauranteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteApiApplication.class, args);
+    }
+
+}
