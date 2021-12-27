12caa68ba6470023964a685f0b5446635f9d49ff
Vinicius Feitoza Rodrigues <vinicius.feitoza@mercadolivre.com>
2021-12-27 16:22:32 -0300

    Adiciona repository da entidade Mesa

---
+++ src/main/java/com/bootcamp/wave/g8/spring3exe1/restauranteapi/repository/MesaRepository.java
@@ -0,0 +1,38 @@
+package com.bootcamp.wave.g8.spring3exe1.restauranteapi.repository;
+
+import com.bootcamp.wave.g8.spring3exe1.restauranteapi.entity.Mesa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MesaRepository implements IRepository<Mesa>{
+
+    private List<Mesa> mesas = new ArrayList<>();
+
+    @Override
+    public void salva(Mesa mesa) {
+        this.mesas.add(mesa);
+    }
+
+    @Override
+    public Mesa buscaUm(Long id) {
+        return this.mesas.stream().filter(mesa -> mesa.getId() == id).findFirst().get();
+    }
+
+    @Override
+    public List<Mesa> buscaTodos() {
+        return this.mesas;
+    }
+
+    @Override
+    public void atualiza(Long id, Mesa mesa) {
+        Mesa p = this.buscaUm(id);
+        p.setListaDePedidos(mesa.getListaDePedidos());
+        p.setValorTotal(mesa.getValorTotal());
+    }
+
+    @Override
+    public void deleta(Long id) {
+        this.mesas.removeIf(mesa -> mesa.getId() == id);
+    }
+}
