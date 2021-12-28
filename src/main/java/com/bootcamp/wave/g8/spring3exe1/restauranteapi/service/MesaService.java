0a6d2d865db0c10e87cc42fc8379db03697bb21d
lbognotti <lucas.bognotti@mercadolivre.com>
2021-12-28 16:08:50 -0300

    Adiciona o metodo de salva mesa

---
+++ src/main/java/com/bootcamp/wave/g8/spring3exe1/restauranteapi/service/MesaService.java
@@ -0,0 +1,31 @@
+package com.bootcamp.wave.g8.spring3exe1.restauranteapi.service;
+
+import com.bootcamp.wave.g8.spring3exe1.restauranteapi.entity.Mesa;
+import com.bootcamp.wave.g8.spring3exe1.restauranteapi.repository.MesaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class MesaService {
+    private MesaRepository mesaRepository;
+
+    public MesaService(MesaRepository mesaRepository) {
+        this.mesaRepository = mesaRepository;
+    }
+
+    public Mesa buscaTodosItemPedidos(Long id) {
+        Mesa mesa = null;
+        try {
+            mesa = this.mesaRepository.buscaUm(id);
+        } catch (NoSuchElementException e) {
+            System.out.println(e);
+        }
+        return mesa;
+    }
+
+    public void salva(Mesa mesa) {
+        this.mesaRepository.salva(mesa);
+    }
+
+}
